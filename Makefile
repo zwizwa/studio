6f7d9d4181532e12e38c47b2d39d7ba09d77d251
Tom Schouten <tom@zwizwa.be>
2016-04-04 13:56:50 -0400

    cleanup

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
 PROJECT = studio
 PROJECT_DESCRIPTION = Erlang Music Studio
 PROJECT_VERSION = 0.0.1
-DEPS = erl_tools
+DEPS = erl_tools erl_sqlite3
 
 C_SRC_TYPE = executable
 LDFLAGS += -lasound -ljack
+
+CFLAGS_ERL_TOOLS := -I$(shell readlink -f deps/erl_tools/c_src)
+CFLAGS += -std=c99 $(CFLAGS_ERL_TOOLS)
+
+
 
 # If a package is not in the index, just add it like this:
 PACKAGES += erl_tools
@@ -12,8 +17,16 @@ pkg_erl_tools_name = erl_tools
 pkg_erl_tools_description = Misc Erlang tools
 pkg_erl_tools_homepage = https://zwizwa.be/git/erl_tools
 pkg_erl_tools_fetch = git
-pkg_erl_tools_repo = http://github.com/zwizwa/erl_tools
+pkg_erl_tools_repo = ~/git/erl_tools
 pkg_erl_tools_commit = master
+
+PACKAGES += erl_sqlite3
+pkg_erl_sqlite3_name = erl_sqlite3
+pkg_erl_sqlite3_description = Minimalistic SQLite3 Erlang Port
+pkg_erl_sqlite3_homepage = ~/git/erl_sqlite3
+pkg_erl_sqlite3_fetch = git
+pkg_erl_sqlite3_repo = ~/git/erl_sqlite3
+pkg_erl_sqlite3_commit = master
 
 include erlang.mk
 
--- c_src/lib.h
+++ c_src/lib.h
@@ -4,9 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
-
-#define LOG(...)   fprintf(stderr,__VA_ARGS__)
-#define ERROR(...) do{LOG(__VA_ARGS__);exit(1);}while(0)
+#incllude "system.h"
 
 #define CT_ASSERT(name, pred) \
     typedef char nct_assert_##name[(pred) ? 1 : -1]
@@ -14,33 +12,6 @@
     CT_ASSERT(struct_##name, sizeof(struct name) == size)
 #define CT_ASSERT_UNION_SIZE(name, size) \
     CT_ASSERT(union_##name, sizeof(union name) == size)
-
-#define ASSERT(assertion) ({ \
-            if(!(assertion)) { \
-                LOG("%s: %d: ASSERT FAIL: " #assertion "\n", __FILE__, __LINE__); \
-                exit(1); \
-            } })
-#define ASSERT_EQ(a,b) ({ \
-            __typeof__(a) _a = (a); \
-            __typeof__(b) _b = (b); \
-            if(_a != _b) { \
-                LOG("ASSERT FAIL: " #a "(%d) == " #b "(%d)\n", _a, _b); \
-                exit(1); \
-            } })
-#define ASSERT_NEQ(a,b) ({ \
-            __typeof__(a) _a = (a); \
-            __typeof__(b) _b = (b); \
-            if(_a == _b) { \
-                LOG("ASSERT FAIL: " #a "(%d) != " #b "(%d)\n", _a, _b); \
-                exit(1); \
-            } })
-#define ASSERT_GT(a,b) ({ \
-            __typeof__(a) _a = (a); \
-            __typeof__(b) _b = (b); \
-            if(_a <= _b) { \
-                LOG("ASSERT FAIL: " #a "(%d) <= " #b "(%d)\n", _a, _b); \
-                exit(1); \
-            } })
 
 #define ARRAY_SIZE(x) \
     (sizeof(x)/sizeof(x[0]))
