11fba8bed3640c47ae9023ec2c71a0e0491b294b
Tom Schouten <tom@zwizwa.be>
2017-07-31 20:33:04 -0400

    notes, ssh

--- src/studio_sup.erl
+++ src/studio_sup.erl
@@ -1,6 +1,6 @@
 -module(studio_sup).
 -behaviour(supervisor).
--export([start_link/0, init/1]).
+-export([start_link/0, init/1, start_ssh/0, start_ssh_shell/2]).
 
 start_link() ->
 	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
@@ -13,3 +13,23 @@ init([]) ->
               [worker(permanent, midi_jack, midi, jackd_port_start_link,[]),
                worker(permanent, midi_hub,  midi, hub_start_link,[]),
                worker(permanent, db,        db,   start_link,[])]}}.
+
+
+start_ssh_shell(_User, _PeerAddr) ->
+    %%GL=erlang:group_leader(),
+    %%tools:info_subscribe(GL),
+    %%tools:info("ssh: ~p~n", [{_User,_PeerAddr}]),  %% Things causes shell to hang.
+    shell:start([]).
+    
+
+start_ssh() ->
+    application:ensure_all_started(ssh),
+    %% Shell is only started if no ssh command is provided.
+    ssh:daemon(
+      2222, 
+      [{shell,       fun studio_sup:start_ssh_shell/2},
+       {system_dir,  priv_dir("ssh_daemon")},
+       {user_dir,    priv_dir("ssh_user/.ssh")}]).
+
+priv_dir(D) ->
+    tools:format("priv/~s", [D]).
