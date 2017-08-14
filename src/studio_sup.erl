5843c9dbdab7593fd9aa949d92c33a8e87a52fc8
Tom Schouten <tom@zwizwa.be>
2017-08-13 22:28:14 -0400

    restore cookie

--- src/studio_sup.erl
+++ src/studio_sup.erl
@@ -1,7 +1,7 @@
 -module(studio_sup).
 -behaviour(supervisor).
--export([start_link/0, init/1
-        %% ,set_cookie/0
+-export([start_link/0, init/1,
+         set_cookie/0
         ]).
 
 start_link() ->
@@ -10,14 +10,13 @@ start_link() ->
 worker(Restart,Name,Mod,Fun,Args) ->
     {Name,{Mod,Fun,Args},Restart,brutal_kill,worker,[Mod]}.
 
-%% set_cookie() ->
-%%     {ok, Bin} = file:read_file(".cookie"),
-%%     erlang:set_cookie(node(), binary_to_atom(Bin,utf8)).
-    
+set_cookie() ->
+    {ok, Bin} = file:read_file(".cookie"),
+    erlang:set_cookie(node(), binary_to_atom(Bin,utf8)).
 
 init([]) ->
     %% use .erlang.cookie instead
-    %% set_cookie(),
+    set_cookie(),
     {ok, {{one_for_one, 1, 5},
           [worker(permanent, midi_jack, midi, jackd_port_start_link,[]),
            worker(permanent, midi_hub,  midi, hub_start_link,[]),
