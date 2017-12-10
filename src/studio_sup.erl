4da76131fb2cde33a63cd4f620584a737ba2e68f
Tom Schouten <tom@zwizwa.be>
2017-12-10 18:12:17 -0500

    cookie

--- src/studio_sup.erl
+++ src/studio_sup.erl
@@ -16,7 +16,7 @@ set_cookie() ->
 
 init([]) ->
     %% use .erlang.cookie instead
-    set_cookie(),
+    %% set_cookie(),
     {ok, {{one_for_one, 1, 5},
           [worker(permanent, midi_jack, midi, jackd_port_start_link,[]),
            worker(permanent, midi_hub,  midi, hub_start_link,[]),
